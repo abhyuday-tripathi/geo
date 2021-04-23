2d8fcc105ca80be0177a2b74eee7541ab9c3a274
Abhyuday Tripathi <tripathi.abhyuday2010@gmail.com>
2021-04-23 18:50:29 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const altitudeEl = document.querySelector('.alt');
+const longitudeEl = document.querySelector('.long');
+const latitudeEl = document.querySelector('.lat');
+const errorEl = document.querySelector('.error');
+
+const success = result => {
+  console.log(result);
+  const altitude = result.coords.altitude;
+  const latitude = result.coords.latitude;
+  const longitude = result.coords.longitude;
+  altitudeEl.textContent = `Altitude: ${altitude}`;
+  latitudeEl.textContent = `Latitude: ${latitude}`;
+  longitudeEl.textContent = `Longitude: ${longitude}`;
+};
+
+const error = err => {
+  errorEl.textContent = err.message;
+};
+
+if ('geolocation' in navigator) {
+  window.navigator.geolocation.getCurrentPosition(success, error);
+}
